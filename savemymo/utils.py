8fd7b01507d34be2b037699367ca656000c16139
Cristovao Coelho <cristovao3g@gmail.com>
2018-03-03 00:05:30 -0300

    Fixed numeric rounding in the pretty_date function.

--- savemymo/utils.py
+++ savemymo/utils.py
@@ -34,22 +34,22 @@ def pretty_date(time=False):
     day_diff = diff.days
 
     if day_diff < 0:
-        return ''
+        return 'N/I'
 
     if day_diff == 0:
         if second_diff < 10:
             return "just now"
         if second_diff < 60:
-            return str(second_diff) + " seconds ago"
+            return "{} seconds ago".format(str(round(second_diff)))
         if second_diff < 120:
             return "a minute ago"
         if second_diff < 3600:
-            return str(second_diff / 60) + " minutes ago"
+            return "{} minutes ago".format(str(round(second_diff / 60)))
         if second_diff < 7200:
             return "an hour ago"
         if second_diff < 86400:
-            return str(second_diff / 3600) + " hours ago"
+            return "{} hours ago".format(str(round(second_diff / 3600)))
     if day_diff == 1:
         return "Yesterday"
     if day_diff < 7:
-        return str(day_diff) + " days ago"
+        return "{} days ago".format(str(round(day_diff)))
